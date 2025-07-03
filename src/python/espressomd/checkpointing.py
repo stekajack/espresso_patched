2238357453dbe86d18d5c198c99bfafb76d9d470
stekajack <avantardejack@gmail.com>
2025-07-03 14:33:52 +0000

    relax checkpoint name restriction

--- src/python/espressomd/checkpointing.py
+++ src/python/espressomd/checkpointing.py
@@ -47,7 +47,7 @@ class Checkpoint:
     def __init__(self, checkpoint_id=None, checkpoint_path="."):
         # check if checkpoint_id is valid (only allow a-z A-Z 0-9 _ -)
         if not isinstance(checkpoint_id, str) or bool(
-                re.compile(r"[^a-zA-Z0-9_\-]").search(checkpoint_id)):
+                re.compile(r"[^a-zA-Z0-9_\-.]").search(checkpoint_id)):
             raise ValueError("Invalid checkpoint id.")
 
         if not isinstance(checkpoint_path, str):
